04c67ad5cb13ffdb5f974fd8f5fffc6da9902097
kaminyv <kaminyv@yandex.ru>
2023-02-12 01:33:23 +0600

    ADDED model Item

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,20 @@
+"""
+API application models module
+"""
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Item(models.Model):
+    """
+    Model represents products
+    """
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.FloatField()
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular item instance.
+        """
+        return reverse('item-detail', args=[str(self.id)])
