1a3cbbf6c1cf864cae6853f8236e69c46ceef79a
kaminyv <kaminyv@yandex.ru>
2023-02-12 14:11:46 +0600

    UPDATE template and view.

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ItemListView, ItemDetailView
+from .views import ItemListView, ItemDetailView, ItemBuyView
 
 urlpatterns = [
     path('', ItemListView.as_view(), name='list-item'),
-    path('item/<pk>', ItemDetailView.as_view(), name='item-detail')
+    path('item/<pk>', ItemDetailView.as_view(), name='item-detail'),
+    path('buy/<pk>', ItemBuyView.as_view(), name='buy-item'),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,11 +1,47 @@
-from django.views.generic import ListView, DetailView
+from django.http import JsonResponse
+from django.views.generic import ListView, DetailView, View
+from django.conf import settings
 from .models import Item
+import stripe
 
 
 class ItemListView(ListView):
+    """
+    Returns the list of volumes of an item.
+    """
     model = Item
 
 
 class ItemDetailView(DetailView):
+    """
+    Returns an item by id.
+    """
     model = Item
 
+
+class ItemBuyView(View):
+    """
+    Returns session id for Stripe by item id.
+    """
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        item = Item.objects.get(pk=pk)
+
+        session = stripe.checkout.Session.create(
+            api_key=settings.STRIPE_API_KEY,
+            line_items=[{
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.name,
+                    },
+                    'unit_amount': int(item.price * 100),
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=request.headers.get('Referer'),
+            cancel_url=request.headers.get('Referer'),
+        )
+
+        return JsonResponse({'session_id': session.get('id')})
